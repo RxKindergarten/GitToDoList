66737f9fcf178888706f1dd394cad877a459bea3
jaeyong <maxwodyd98@gmail.com>
2021-04-17 16:49:39 +0900

    create: Tab 폴더 생성

---
+++ GitToDoList/Screen/Tab/TabVC.swift
@@ -0,0 +1,17 @@
+//
+//  TabVC.swift
+//  GitToDoList
+//
+//  Created by 이재용 on 2021/04/17.
+//
+
+import UIKit
+
+class TabVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
