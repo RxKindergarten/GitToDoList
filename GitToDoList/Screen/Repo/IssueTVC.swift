22abc0a3fedc7c0c8ab20fc95d79454bb7eb1c7e
jaeyong <maxwodyd98@gmail.com>
2021-05-15 11:53:55 +0900

    update: issue cell UI구성

---
+++ GitToDoList/Screen/Repo/IssueTVC.swift
@@ -0,0 +1,16 @@
+//
+//  IssueTVC.swift
+//  GitToDoList
+//
+//  Created by 이재용 on 2021/05/12.
+//
+
+import UIKit
+
+class IssueTVC: UITableViewCell {
+    static let identifier: String = "IssueTVC"    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var labelLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+}
--- GitToDoList/Screen/Repo/RepoVC.swift
+++ GitToDoList/Screen/Repo/RepoVC.swift
@@ -7,28 +7,49 @@
 
 import UIKit
 
-class RepoVC: UIViewController {
+struct Issue {
+    var title: String
+    var subTitle: String
+    var label: String
+}
 
-    @IBOutlet weak var repoTableView: UITableView!
+enum CellType {
+    case header
+    case issue
+}
+class RepoVC: UIViewController {
+    
+    var issues: [Issue] = [
+        Issue(title: "[Repo] 레포별 이슈리스트 UI 구성", subTitle: "#5 opened 1 hour ago by wody27", label: "재용"),
+        Issue(title: "[Repo] 레포별 이슈리스트 UI 구성", subTitle: "#5 opened 1 hour ago by wody27", label: "재용"),
+        Issue(title: "[Repo] 레포별 이슈리스트 UI 구성", subTitle: "#5 opened 1 hour ago by wody27", label: "재용"),
+        Issue(title: "[Repo] 레포별 이슈리스트 UI 구성", subTitle: "#5 opened 1 hour ago by wody27", label: "재용")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         repoTableView.delegate = self
         repoTableView.dataSource = self
         repoTableView.rowHeight = UITableView.automaticDimension
         repoTableView.estimatedRowHeight = 44.0
     }
     
+    @IBOutlet weak var repoTableView: UITableView!
+    
 }
 
 extension RepoVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return issues.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepoTitleTVC.identifier) as? RepoTitleTVC else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: IssueTVC.identifier) as? IssueTVC else { return UITableViewCell() }
         
+        cell.titleLabel.text = issues[indexPath.row].title
+        cell.subTitleLabel.text = issues[indexPath.row].subTitle
+        cell.labelLabel.text = issues[indexPath.row].label
         return cell
     }
     
